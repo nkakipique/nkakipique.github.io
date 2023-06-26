d8a8468d3396048868c6d042d7d2df138e29ba85
nkakipique <pakonkaki47@gmail.com>
2023-06-26 15:41:13 +0200

    Added new files

---
+++ js/javaScript.js
@@ -0,0 +1,7 @@
+/* ======== typing animation ========== */
+var typed = new Typed(".typing", {
+    strings:["", "Software Developer", "web Developer", "Android App Developer", "Desktop App Developer"],
+    typeSpeed:100,
+    BackSpeed: 60,
+    loop:true
+})
